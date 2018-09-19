7fb0820ebccb2e026162fb5feb13bdcc4e64011e
Rayapati,Naresh <naresh.rayapati@yahoo.com>
2018-09-19 07:59:50 -0500

    Strategy Pattern with Clojure
    - Generally we do know how to do this in Java but if we have do it in
    Clojure.

---
+++ 2018/clojure/strategy_pattern.clj
@@ -0,0 +1,18 @@
+;; implementing comparator in Clojure, soring users by subscription. cerner_2^5_2018
+;; User has subscription and each subscription have a name, to sort these users by subscription name.
+(sort (comparator
+       (fn [u1 u2]
+         (cond
+          (= (:subscription u1)
+             (:subscription u2)) (neg? (compare (:name u1)
+                                                (:name u2)))
+             (:subscription u1) true
+             :else false))) users)
+
+;; or simple format with juxt
+
+;; forward sort
+(sort-by (juxt (complement :subscription) :name) users)
+
+;; reverse sort
+(sort-by (juxt :subscription :name) #(compare %2 %1) users)
