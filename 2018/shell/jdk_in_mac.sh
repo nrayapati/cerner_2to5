d684a4e409b71b252b6977123a841597f3757005
Rayapati,Naresh <naresh.rayapati@yahoo.com>
2018-09-26 08:44:37 -0500

    JDKs in MAC

---
+++ 2018/shell/jdk_in_mac.sh
@@ -0,0 +1,10 @@
+# cerner_2^5_2018
+# Here is a command to list down JDK versions those are installed and available on your mac. 
+$ /usr/libexec/java_home -V  
+# Matching Java Virtual Machines (4):  
+#     1.8.0_101, x86_64: "Java SE 8" /Library/Java/JavaVirtualMachines/jdk1.8.0_101.jdk/Contents/Home  
+#     1.7.0_79, x86_64: "Java SE 7" /Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home  
+#     1.6.0_65-b14-468, x86_64: "Java SE 6" /Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home  
+#     1.6.0_65-b14-468, i386: "Java SE 6" /Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+
+# The list shows what JDK versions you have and the final line shows which is currently the default version.
