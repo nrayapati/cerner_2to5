6594f3627a06f568af0b6437f328255c84201e9b
Naresh Rayapati <naresh.rayapati@yahoo.com>
2018-10-05 05:52:29 -0500

    Transposing matrix in Python

---
+++ 2018/python/transposing_matrix.py
@@ -0,0 +1,20 @@
+# Reverse a String
+
+a =  "cerner_2^5_2018"
+print "Reverse is",a[::-1]
+# Reverse is 8102_5^2_renrec
+
+# Transposing a Matrix
+mat = [[1, 2, 3], [4, 5, 6]]
+zip(*mat)
+# [(1, 4), (2, 5), (3, 6)]
+
+# Swap two numbers with one line of code.
+
+a=7
+b=5
+b, a =a, b
+print a
+# 5
+print b
+# 7
